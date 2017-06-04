e6d7f8484e9feb1b3ae1008a5704ce84633f304d
Ethan Li <lietk12@gmail.com>
2017-06-03 18:51:56 -0700

    Resize captured images to 320x240.

--- stereovision/stereo_cameras.py
+++ stereovision/stereo_cameras.py
@@ -58,6 +58,9 @@ class StereoPair(object):
         if devices[0] != devices[1]:
             #: Video captures associated with the ``StereoPair``
             self.captures = [cv2.VideoCapture(device) for device in devices]
+            for capture in self.captures:
+                capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 320)
+                capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 240)
         else:
             # Stereo images come from a single device, as single image
             self.captures = [cv2.VideoCapture(devices[0])]
